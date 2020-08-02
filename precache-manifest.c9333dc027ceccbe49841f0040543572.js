f4fe069577fb018125c820c81cc00658eeeb5c3c
rseligm2 <rseligm2@illinois.edu>
2020-08-02 18:57:04 -0500

    Updates

---
+++ precache-manifest.c9333dc027ceccbe49841f0040543572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace4864b15069c5441f862915751594e",
+    "url": "/DataVisualizationProject/index.html"
+  },
+  {
+    "revision": "3390ed93b71397e10b4e",
+    "url": "/DataVisualizationProject/static/css/main.7ac23cee.chunk.css"
+  },
+  {
+    "revision": "d0e2b1d866c0bdf32860",
+    "url": "/DataVisualizationProject/static/js/2.8ae9b604.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/DataVisualizationProject/static/js/2.8ae9b604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3390ed93b71397e10b4e",
+    "url": "/DataVisualizationProject/static/js/main.4c9e91fe.chunk.js"
+  },
+  {
+    "revision": "69009d4e50e4e0f30171",
+    "url": "/DataVisualizationProject/static/js/runtime-main.b7911d4c.js"
+  },
+  {
+    "revision": "3af28ac95b31dcf8d14d6a10aa6f4104",
+    "url": "/DataVisualizationProject/static/media/majordata.3af28ac9.csv"
+  }
+]);
